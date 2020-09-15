a55805862e31e71e5fbbb7dd2ebb1ebec6becd08
Albert-Wu-03 <22qwu@athenian.org>
2020-09-15 03:15:45 +0000

    Binary

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,12 +3,22 @@ import java.util.Scanner;
 public class MyMain {
     
     public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
+        int x = Integer.parseInt(binary);
+        int i = 1;
+        int d = 0;
+        for(int l=0; l < binary.length(); l++){
+            d += (x%10)*i;
+            x /= 10;
+            i*=2;
+        }
+        return d;
     }
     
     
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Type a number in binary format");
+        String binary = scan.nextLine();     
+        System.out.println("That is equal to the decimal value "+binaryToDecimal(binary));
     }
 }
